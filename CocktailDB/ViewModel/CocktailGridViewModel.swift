555d89bb3e6a9f5eeb325cc9e7345c8701fc233f
Tatiana Carvajal <taticv159912@gmail.com>
2024-06-17 18:29:10 -0500

    Adding fetchCocktailCategories function to the ViewModel

--- CocktailDB/ViewModel/CocktailGridViewModel.swift
+++ CocktailDB/ViewModel/CocktailGridViewModel.swift
@@ -8,5 +8,29 @@
 import Foundation
 
 class CocktailGridViewModel {
+    enum State {
+        case loading
+        case error(ServiceError)
+        case idle
+        case loadedCategories(CategoriesResponse)
+        case loadedCocktails(CocktailResponse)
+    }
     
+    let service: CocktailServiceProtocol
+    var state = State.idle
+    
+    init(service: CocktailServiceProtocol) {
+        self.service = service
+    }
+    
+    func fetchCocktailCategories() async {
+        state = .loading
+        do {
+            var categories = try await service.fetchCocktailCategories()
+            state = .loadedCategories(categories)
+        }
+        catch {
+            state = .error(.noDataFound)
+        }
+    }
 }
