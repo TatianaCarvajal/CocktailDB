d428d93fd149e14084232104ff4090214b168612
Tatiana Carvajal <taticv159912@gmail.com>
2024-06-18 16:27:03 -0500

    Creating fetchCocktailCategoriesFailure test

--- CocktailDBTests/CocktailGridViewModelTests.swift
+++ CocktailDBTests/CocktailGridViewModelTests.swift
@@ -9,16 +9,30 @@ import XCTest
 @testable import CocktailDB
 
 final class CocktailGridViewModelTests: XCTestCase {
-
+    
     func testFetchCocktailCategories() async {
-            let cocktailGridViewModel = CocktailGridViewModel(service: CocktailServiceProtocolMock())
-            
-            await cocktailGridViewModel.fetchCocktailCategories()
-            
-            switch cocktailGridViewModel.state {
-            case let .loadedCategories(categories):
-                XCTAssertEqual(categories, .init(drinks: [.init(category: "Ordinary Drink")]))
-            default: XCTFail("This shouldn't happen because the mock has a default success")
-            }
+        let cocktailGridViewModel = CocktailGridViewModel(service: CocktailServiceProtocolMock())
+        
+        await cocktailGridViewModel.fetchCocktailCategories()
+        
+        switch cocktailGridViewModel.state {
+        case let .loadedCategories(categories):
+            XCTAssertEqual(categories, .init(drinks: [.init(category: "Ordinary Drink")]))
+        default: XCTFail("This shouldn't happen because the mock has a default success")
         }
+    }
+    
+    func testFetchCocktailCategoriesFailure() async {
+        let cocktailServiceProtocolMock = CocktailServiceProtocolMock()
+        cocktailServiceProtocolMock.withSuccess = false
+        let cocktailGridViewModel = CocktailGridViewModel(service: cocktailServiceProtocolMock)
+        
+        await cocktailGridViewModel.fetchCocktailCategories()
+        
+        switch cocktailGridViewModel.state {
+        case let .error(error):
+            XCTAssertEqual(error, .noDataFound)
+        default: XCTFail("This shouldn't happen because the mock should always fail")
+        }
+    }
 }
